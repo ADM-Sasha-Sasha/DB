d9fa823d7b9890845f1e6f5d5466852c2f1eb335
ADM-Sasha-Sasha <webi9876@gmail.com>
2021-08-02 17:56:28 +0300

    test

--- HW-3.1/initDB.sql
+++ HW-3.1/initDB.sql
@@ -6,8 +6,6 @@ CREATE DATABASE data_base_for_hw3
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Polish_Poland.1250'
-    LC_CTYPE = 'Polish_Poland.1250'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
