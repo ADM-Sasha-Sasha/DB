0f6bfafd4f4c1e88ea101ae78d3cda8d395996bc
ADM-Sasha-Sasha <webi9876@gmail.com>
2021-08-31 09:31:04 +0300

    test

--- HW-3.1/populateDB.sql
+++ HW-3.1/populateDB.sql
@@ -1,82 +1,74 @@
-insert into companies (company_id, company_name, number_of_developers, customer_id, project_id)
-values (1, 'Epam', null, null, null);
-insert into companies (company_id, company_name, number_of_developers, customer_id, project_id)
-values (2, 'GOIT', null, null, null);
-insert into companies (company_id, company_name, number_of_developers, customer_id, project_id)
-values (3, 'Microsoft', null, null, null);
-insert into companies (company_id, company_name, number_of_developers, customer_id, project_id)
-values (4, 'Apple', null, null, null);
-insert into companies (company_id, company_name, number_of_developers, customer_id, project_id)
-values (5, 'CD Project', null, null, null);
+USE initDB;
 
-insert into projects values (1, null, 5, null, 'Best project', 'new', 5, 1000000);
-insert into projects values (2, null, 5, null, 'Proj name 2', 'new', 5, 1000000);
-insert into projects values (3, null, 5, null, 'Proj name 2', 'pending', 5, 10000);
-insert into projects values (4, null, 5, null, 'Proj name 3', 'new', 5, 100000);
-insert into projects values (5, null, 5, null, 'Proj name 4', 'new', 5, 5000000);
-insert into projects values (6, null, 5, null, 'Proj name 5', 'new', 5, 6000000);
-insert into projects values (7, null, 5, null, 'Proj name 6', 'new', 5, 7000000);
-insert into projects values (8, null, 5, null, 'Proj name 7', 'new', 5, 80000);
-insert into projects values (9, null, 5, null, 'Proj name 8', 'new', 5, 1000);
+INSERT INTO developers VALUES
+(1, 'John Doe', 25, 'male'),
+(2, 'Kryakva Batcovich', 21, 'female'),
+(3, 'Gus Batcovich', 33, 'male'),
+(4, 'Ivan Ivanovich', 35, 'male'),
+(5, 'Vasya Pupkin', 20, 'male'),
+(6, 'John Smith', 22, 'male'),
+(7, 'Maria Ivanovna', 33, 'female'),
+(8, 'Bags Bunny', 19, 'male'),
+(9, 'Daffy Duck', 25, 'male'),
+(10, 'Gomer Simpson', 40, 'male'),
+(11, 'Lola Bunny', 18, 'female');
 
-insert into developers (developer_id, first_name, last_name, gender, age, experience_in_years, company_id, project_id)
-values (1, 'Ivan', 'Petrov', 'male', 30, 10, 5, 1);
-insert into developers (developer_id, first_name, last_name, gender, age, experience_in_years, company_id, project_id)
-values (2, 'Petro', 'Ivanov', 'male', 30, 10, 4, 3);
-insert into developers (developer_id, first_name, last_name, gender, age, experience_in_years, company_id, project_id)
-values (3, 'Olexandr', 'Puszkin', 'male', 30, 10, 5, 2);
-insert into developers (developer_id, first_name, last_name, gender, age, experience_in_years, company_id, project_id)
-values (4, 'Anton', 'Gogol', 'male', 30, 10, 2, 4);
-insert into developers (developer_id, first_name, last_name, gender, age, experience_in_years, company_id, project_id)
-values (5, 'Serhii', 'Bulba', 'male', 30, 10, 1, 5);
+INSERT INTO skills VALUES 
+(1, 'Java','Junior'),
+(2, 'Java','Middle'),
+(3, 'Java','Senior'),
+(4, 'Kotlin','Junior'),
+(5, 'Kotlin','Middle'),
+(6, 'Kotlin','Senior'),
+(7, 'Python','Junior'),
+(8, 'Python','Middle'),
+(9, 'Python','Senior'),
+(10, 'C++','Junior'),
+(11, 'C++','Middle'),
+(12, 'C++','Senior'),
+(13, 'JavaScript','Junior'),
+(14, 'JavaScript','Middle'),
+(15, 'JavaScript','Senior');
 
-insert into skills values (1, 'Java', 'Junior');
-insert into skills values (2, 'C++', 'Junior');
-insert into skills values (1, 'JS', 'Middle');
-insert into skills values (3, 'C++', 'Junior');
-insert into skills values (4, 'JS', 'Senior');
-insert into skills values (5, 'JS', 'Junior');
-insert into skills values (5, 'Java', 'Junior');
-insert into skills values (1, 'Java', 'Junior');
+INSERT INTO projects VALUES
+(1, 'Assassins Creed', 'Gaming'),
+(2, 'Mobile Bank', 'Bank'),
+(3, 'Weather Prognose', 'Radio'),
+(4, 'Robotics', 'Machine Learning'),
+(5, 'Grand Theft Auto V', 'Gaming'),
+(6, 'Desing', 'Gaming'),
+(7, 'Android ', 'Mobile operating system ');
 
-insert into customers values (1, 'Customer', 1);
-insert into customers values (2, 'Customer9', 1);
-insert into customers values (3, 'Customer2', 2);
-insert into customers values (4, 'Customer3', 4);
-insert into customers values (5, 'Customer4', 1);
-insert into customers values (6, 'Customer5', 3);
-insert into customers values (7, 'Customer6', 7);
-insert into customers values (8, 'Customer7', 6);
-insert into customers values (9, 'Customer8', 8);
 
-insert into developers_on_projects values (1, 1);
-insert into developers_on_projects values (1, 5);
-insert into developers_on_projects values (1, 2);
-insert into developers_on_projects values (2, 5);
-insert into developers_on_projects values (2, 3);
-insert into developers_on_projects values (3, 5);
-insert into developers_on_projects values (3, 3);
-insert into developers_on_projects values (3, 4);
-insert into developers_on_projects values (3, 1);
-insert into developers_on_projects values (4, 5);
-insert into developers_on_projects values (5, 5);
-insert into developers_on_projects values (6, 5);
-insert into developers_on_projects values (7, 5);
-insert into developers_on_projects values (8, 5);
-insert into developers_on_projects values (8, 4);
-insert into developers_on_projects values (8, 2);
+INSERT INTO companies VALUES 
+(1, 'N-iX', 'Lviv'),
+(2, 'Ubisoft Montreal', 'Montreal'),
+(3, 'Rockstar Games', 'New-York'),
+(4, 'Boston Dynamics', 'Waltham'),
+(5, 'Google','Mountain View'),
+(6,'Itransition','Kiyv');
 
-insert into customers_and_companies values (1, 1, 8);
-insert into customers_and_companies values (1, 2, 7);
-insert into customers_and_companies values (2, 2, 6);
-insert into customers_and_companies values (3, 4, 5);
-insert into customers_and_companies values (4, 5, 4);
-insert into customers_and_companies values (5, 2, 1);
-insert into customers_and_companies values (6, 1, 1);
-insert into customers_and_companies values (6, 2, 1);
-insert into customers_and_companies values (7, 2, 8);
-insert into customers_and_companies values (7, 5, 1);
-insert into customers_and_companies values (7, 4, 7);
-insert into customers_and_companies values (8, 2, 4);
-insert into customers_and_companies values (3, 2, 6);
-insert into customers_and_companies values (5, 1, 2);
+INSERT INTO customers VALUES 
+(1, 'Universal Bank','Kyiv','Bank'),
+(2, 'CRM','Chernigiv','News'),
+(3, 'Samsung', 'Seoul','Conglomerate'),
+(4,'Konami','Tokyo', 'Video Games'),
+(5, 'Gaming players', 'All World','Gaming');
+
+INSERT INTO dev_skills VALUES 
+(1,3),(1,11),(2,13),(3,3),(3,5),(4,3),(4,8),(4,6),(5,13),(5,9),(6,5),
+(6,10),(7,15),(7,11),(8,2),(8,12),(9,1),(9,7),(10,9),(10,4),(11,12),(11,14);
+
+INSERT INTO dev_projects VALUES
+(1,5),(2,7),(3,7),(4,4),(5,4),(5,3),(5,6),(6,1),(6,5),(7,1),(7,2),(7,6),(8,1),(8,2),(8,5),(9,4),(10,7),(11,5);
+
+INSERT INTO dev_companies VALUES 
+(5,1),(7,1),(6,2),(7,2),(8,2),(1,3),(6,3),(8,3),(11,3),(4,4),(5,4),(9,4),
+(3,5),(10,5),(2,5),(5,6),(7,6),(8,6);
+
+INSERT INTO companies_projects VALUES
+(1,2),(2,6),(3,1),(4,4),(5,3),(6,6),(5,7);
+
+INSERT INTO customers_projects VALUES
+(1,2),(1,7),(2,3),(2,2),(3,7),(3,4),(3,6),(4,7),(4,6),(5,1),(5,5);
+
